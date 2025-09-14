1d04d669387e9ef1c4a90058a1d515e3075fee81
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2025-09-14 16:31:58 -0700

    File Management Adjustments
    Renamed and moved various files after merge.

--- Calypso/Assets/Scripts/Combat Systems/DamageCalculator.cs
+++ Calypso/Assets/Scripts/Combat Systems/DamageCalculator.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DamageController : MonoBehaviour
+public class DamageCalculator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
