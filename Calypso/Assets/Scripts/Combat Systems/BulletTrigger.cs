62b516c8f5cfe3bc3e97274e7acbab4009c0eda3
Donald Paladino <106718081+DUBBEP@users.noreply.github.com>
2025-09-15 12:55:08 -0700

    Cleanup

--- Calypso/Assets/Scripts/Combat Systems/BulletTrigger.cs
+++ Calypso/Assets/Scripts/Combat Systems/BulletTrigger.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class BulletTrigger : MonoBehaviour
 {
     public int bulletDamage = 1;
     void OnParticleCollision(GameObject other)
+    {
+        TryDamage(other);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TryDamage(other.gameObject);
+    }
+
+    private void TryDamage(GameObject other)
     {
         if (other.GetComponent<HealthSystem>() != null)
         {
             other.GetComponent<HealthSystem>().TakeDamage(bulletDamage);
         }
     }
-
 }
