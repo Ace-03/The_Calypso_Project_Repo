b571f230f6120d810e746588465c3dae049f4313
Donald Paladino <106718081+DUBBEP@users.noreply.github.com>
2025-09-16 15:35:20 -0700

    Spawn Manager and SO

--- Calypso/Assets/Scripts/Player/Controller/PlayerController.cs
+++ Calypso/Assets/Scripts/Player/Controller/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
     private float baseMaxSpeed;
     private float baseAcceleration;
     private float baseDeceleration;
+    private float slideClamp;
 
     private float maxSpeed;
     private float acceleration;
     private float deceleration;
+
+    private float velocity;
 
     private float rbSpeedAdjustment = 20f;
     private Rigidbody rb;
@@ -29,6 +32,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        velocity = rb.linearVelocity.magnitude;
         Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
 
         if (movementVector.magnitude > 0)
@@ -37,8 +41,9 @@ public class PlayerController : MonoBehaviour
             Vector3 velocityChange = targetVelocity - horizontalVelocity;  
             rb.AddForce(velocityChange * acceleration * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
-        else if (movementVector.magnitude <= 0.15f && horizontalVelocity.magnitude > 0)
+        else if (movementVector.magnitude <= 0.15f && horizontalVelocity.magnitude > slideClamp)
         {
+            Debug.Log("Decelerating");
             Vector3 brakingForce = -horizontalVelocity.normalized * deceleration * (rbSpeedAdjustment / 3);
             rb.AddForce(brakingForce, ForceMode.Acceleration);
         }
@@ -46,6 +51,12 @@ public class PlayerController : MonoBehaviour
         if (horizontalVelocity.magnitude > maxSpeed)
         {
             rb.linearVelocity = horizontalVelocity.normalized * maxSpeed;
+        }
+
+        if (horizontalVelocity.magnitude <= slideClamp)
+        {
+            Debug.Log("Clamping velocity to zero");
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
         }
     }
     
@@ -73,6 +84,7 @@ public class PlayerController : MonoBehaviour
         baseMaxSpeed = PlayerStats.Instance.GetMaxSpeed();
         baseAcceleration = PlayerStats.Instance.GetAcceleration();
         baseDeceleration = PlayerStats.Instance.GetDeceleration();
+        slideClamp = 0.3f;
     }
 
     public void ApplyMovementModifiers()
---
+++ Calypso/Assets/Scripts/SpawnSystem/SpawnManager.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static WaveDefinitionSO;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<WaveDefinitionSO> waveSequence;
+    public Transform playerTransform;
+
+    private int currentWaveIndex = 0;
+    private float waveTimer = 0f;
+
+    private void Start()
+    {
+        waveTimer = waveSequence[0].waveDuration;
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private void Update()
+    {
+        if (waveTimer <= 0)
+        {
+            currentWaveIndex++;
+            if (currentWaveIndex < waveSequence.Count)
+            {
+                waveTimer = waveSequence[currentWaveIndex].waveDuration;
+                StartCoroutine(SpawnEnemies());
+            }
+            else
+            {
+                Debug.Log("All waves completed!");
+            }
+        }
+    }
+
+    private string SpawnEnemies()
+    {
+        while (true)
+        {
+            WaveDefinitionSO currentWave = waveSequence[currentWaveIndex];
+
+            foreach (var enemySpawnInfo in currentWave.enemiesInWave)
+            {
+                float spawnInterval = 1f / enemySpawnInfo.spawnRate;
+
+                if (GameObject.FindGameObjectsWithTag("Enemy").Length < enemySpawnInfo.maxActiveEnemies)
+                {
+                    SpawnEnemy();
+                }
+            }   
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+
+        // spawn from object pool
+        //GameObject enemy = ObjectPool.Instance.GetPooledObject("Enemy");
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        return playerTransform.position + new Vector3(UnityEngine.Random.Range(-20, 20), 0, UnityEngine.Random.Range(-10, 10));
+    }
+}
