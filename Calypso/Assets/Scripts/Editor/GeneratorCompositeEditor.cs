d431f0f36011fac773227534bfdcdf677ae83a01
Donald Paladino <106718081+DUBBEP@users.noreply.github.com>
2025-09-30 13:10:08 -0700

    Demo in Progress

--- Calypso/Assets/Scripts/Editor/GeneratorCompositeEditor.cs
+++ Calypso/Assets/Scripts/Editor/GeneratorCompositeEditor.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 [CustomEditor(typeof(PropGeneratorComposite))]
 public class GeneratorCompositeEditor : Editor
 {
+    private bool usingExclusionZones;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
         PropGeneratorComposite composite = (PropGeneratorComposite)target;
 
+        usingExclusionZones = composite.usingExclusionZones;
+
         if (GUILayout.Button("Regenerate Props"))
             composite.RegenerateAllProps();
 
         if (GUILayout.Button("Clear Props In Editor"))
             composite.ClearAllProps();
+
+        if (GUILayout.Button("Toggle Use Exclusion Zones (Currently " + usingExclusionZones + ")"))
+        {
+            usingExclusionZones = composite.ToggleExclusionZones();
+        }
     }
 }
