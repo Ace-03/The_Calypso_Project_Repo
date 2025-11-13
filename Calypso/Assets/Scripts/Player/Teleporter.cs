fc2c43650c117c764bc02b7820327a3880f4ff9c
Sachiterasu <37966000+SachihoWolfy@users.noreply.github.com>
2025-11-13 15:12:26 -0700

    TeleporterUpdate
    More teleporter options

--- Calypso/Assets/Scripts/Player/Teleporter.cs
+++ Calypso/Assets/Scripts/Player/Teleporter.cs
@@ -6,6 +6,10 @@ public class Teleporter : MonoBehaviour
     public KeyCode teleportHomeButton = KeyCode.H;
     public Transform homeWaypoint;
     public float verticleOffset;
+    public float teleportDelay = 1f;
+    float teleportTimer = 0f;
+    public bool teleporting = false;
+    public ParticleSystem teleParticles;
     Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,9 +21,33 @@ public class Teleporter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(teleportHomeButton))
+        if (Input.GetKeyUp(teleportHomeButton) && !teleporting)
         {
-            TeleportHome();
+            StartTimer();
+        }
+        UpdateTimer();
+    }
+    void StartTimer()
+    {
+        teleportTimer = teleportDelay;
+        teleporting = true;
+        if (teleParticles != null) teleParticles.Play();
+    }
+    void UpdateTimer()
+    {
+        if (teleporting && teleportTimer > 0)
+        {
+            teleportTimer -= Time.deltaTime;
+            if (teleportTimer <= 0)
+            {
+                TeleportHome();
+                if(teleParticles  != null) teleParticles.Pause();
+                teleporting = false;
+            }
+        }
+        if(teleportDelay == 0f && teleporting)
+        {
+            teleportTimer = 0.25f;
         }
     }
 
